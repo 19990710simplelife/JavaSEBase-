9f71ff42d1edae54af4e56cb6fa52f074dc2ee99
19990710simplelife <1753848616@qq.com>
2022-07-30 10:27:47 +0800

    泛型在集合中的使用

---
+++ day02集合框架/src/cn/simplelife/work/_02_generarraylist/GenderArrayListTest.java
@@ -0,0 +1,19 @@
+package cn.simplelife.work._02_generarraylist;
+
+import java.util.ArrayList;
+
+/**
+ * @ClassName GenderArrayListTest
+ * @Description
+ * @Author simplelife
+ * @Date 2022/7/30 9:50
+ * @Version 1.0
+ */
+
+public class GenderArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("123");
+        System.out.println(arrayList);
+    }
+}
