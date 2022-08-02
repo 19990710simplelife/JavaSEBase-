b62304750a80c8f730faf0876da38dfbc0a92998
19990710simplelife <1753848616@qq.com>
2022-08-02 17:23:32 +0800

    throw

---
+++ day04异常处理/src/cn/simplelife/work/_07_throw/ThrowExceptionDemo.java
@@ -0,0 +1,29 @@
+package cn.simplelife.work._07_throw;
+
+/**
+ * @ClassName ThrowExceptionDemo
+ * @Description
+ * @Author simplelife
+ * @Date 2022/8/2 17:10
+ * @Version 1.0
+ */
+
+public class ThrowExceptionDemo {
+    public static boolean exisUser(String username) throws Exception {
+        String[] arr = {"张三", "李四", "王五"};
+        for (String s : arr) {
+            if (username.equals(s)) {
+                throw new Exception("用户名已注册！");
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        try {
+            exisUser("张三");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
